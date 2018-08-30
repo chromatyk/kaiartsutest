0f8ed59bd8528d7dfcf1352942fcdd2b9108ffaf
Lexae <atparky@outlook.fr>
2018-08-30 20:13:47 +0200

    Equipe modifiee

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+$('#circleDrop1').click(function(){
+  $('.card-middle1').slideToggle();
+  $('.close1').toggleClass('closeRotate');
+});
+
+$('#circleDrop').click(function(){
+  $('.card-middle').slideToggle();
+  $('.close').toggleClass('closeRotate');
+});
